e6a56c325559873a1d6c0fed9894ef53f6c1ce3a
songa <thddk0121@gmail.com>
2025-09-14 02:05:09 +0900

    chore: set up dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +14,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  define: {
+    'process.env.REACT_APP_API_BASE_URL': JSON.stringify(
+      process.env.REACT_APP_API_BASE_URL
+    )
   }
 })
