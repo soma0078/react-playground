fe535958de3b9c0405cf1adfd281f75ef83df28a
songa <thddk0121@gmail.com>
2025-09-14 17:05:25 +0900

    feat: add search type selector

--- src/components/table/Filters.tsx
+++ src/components/table/Filters.tsx
@@ -1,5 +1,13 @@
 import type { ColumnFiltersState } from '@tanstack/react-table'
 import { Input } from '../ui/input'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '../ui/select'
+import { useState, type ChangeEvent } from 'react'
 
 interface FiltersProps {
   columnFilters: ColumnFiltersState
@@ -10,8 +18,7 @@ export default function Filters({
   columnFilters,
   setColumnFilters
 }: FiltersProps) {
-  const taskName = (columnFilters.find((f) => f.id === 'task')?.value ||
-    '') as string
+  const [selectedValue, setSelectedValue] = useState('')
 
   const onFilterChange = (id: string, value: string) =>
     setColumnFilters(
@@ -23,12 +30,38 @@ export default function Filters({
         })
     )
 
+  const handleSelectChange = (value: string) => {
+    setColumnFilters([])
+    setSelectedValue(value)
+  }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (selectedValue) {
+      onFilterChange(selectedValue, e.target.value)
+    }
+  }
+
+  const inputValue =
+    (columnFilters.find((f) => f.id === selectedValue)?.value as string) || ''
+
   return (
-    <Input
-      placeholder="Filter tasks..."
-      value={taskName}
-      onChange={(e) => onFilterChange('task', e.target.value)}
-      className="max-w-sm"
-    />
+    <div className="flex gap-2">
+      <Select value={selectedValue} onValueChange={handleSelectChange}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Search type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="task">Task</SelectItem>
+          <SelectItem value="email">Email</SelectItem>
+        </SelectContent>
+      </Select>
+      <Input
+        placeholder="Search texts...."
+        value={inputValue}
+        onChange={handleInputChange}
+        disabled={!selectedValue}
+        className="max-w-sm"
+      />
+    </div>
   )
 }
