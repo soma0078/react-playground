efa7c7190c2ae24acd2699555ed2c070dcc00c65
soma0078 <thddk0121@gmail.com>
2026-02-03 13:31:38 +0900

    feat: TopBanner 추가하여 header sticky 테스트

---
+++ src/components/TopBanner.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="flex h-[52px] items-center justify-center bg-green-200">
+      TopBanner
+    </div>
+  )
+}
--- src/routes/layouts/Default.tsx
+++ src/routes/layouts/Default.tsx
@@ -1,11 +1,13 @@
 import { Outlet, ScrollRestoration } from 'react-router'
 import { TheHeader } from '../../components/TheHeader'
+import TopBanner from '@/components/TopBanner'
 
 export const DefaultLayout = () => {
   return (
     <>
+      <TopBanner />
       <TheHeader />
-      <main className="relative z-1">
+      <main className="relative z-1 min-h-screen">
         <Outlet />
       </main>
       <ScrollRestoration />
